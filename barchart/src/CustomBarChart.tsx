ea275c339bb7d7e7c92a0a2ebbe3faf79412a2f8
coder-saim <saim.cse.du.744@gmail.com>
2023-07-03 11:32:47 +0600

    bar color added

--- barchart/src/CustomBarChart.tsx
+++ barchart/src/CustomBarChart.tsx
@@ -1,4 +1,60 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const datam = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 interface DataPoint {
   name: string; //x-axis label name, eg. Random, Toplist
@@ -13,8 +69,96 @@ interface SingleDataPointGraph {
 
 interface CustomBarChartProps extends SingleDataPointGraph {}
 
-const CustomBarChart = ({ data, chartTitle }: CustomBarChartProps) => {
-  return <div>bargrah</div>;
+function CustomizedAxisTick(props: any) {
+  const { x, y, stroke, payload } = props;
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <text
+        x={0}
+        y={0}
+        dy={16}
+        textAnchor="end"
+        fill="#666"
+        transform="rotate(-90)"
+      >
+        {payload.value}
+      </text>
+    </g>
+  );
+}
+
+const getIntroOfPage = (label: any) => {
+  if (label === "Page A") {
+    return "Page A is about men's clothing";
+  }
+  if (label === "Page B") {
+    return "Page B is about women's dress";
+  }
+  if (label === "Page C") {
+    return "Page C is about women's bag";
+  }
+  if (label === "Page D") {
+    return "Page D is about household goods";
+  }
+  if (label === "Page E") {
+    return "Page E is about food";
+  }
+  if (label === "Page F") {
+    return "Page F is about baby food";
+  }
+  return "";
+};
+
+const CustomTooltip = (props: any) => {
+  const { active, payload, label } = props;
+
+  if (active && payload && payload.length) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="intro">{getIntroOfPage(label)}</p>
+        <p className="desc">Anything you want can be displayed here.</p>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+const CustomBarChart = ({ chartTitle, data }: CustomBarChartProps) => {
+  return (
+    <ResponsiveContainer width="100%" height={600}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis
+          dataKey="name"
+          height={70}
+          allowDuplicatedCategory={false}
+          tick={<CustomizedAxisTick />}
+        />
+
+        <YAxis />
+
+
+        <Tooltip content={<CustomTooltip />} />
+
+        <Bar dataKey="value">
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} />
+          ))}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default CustomBarChart;
--- linechart/src/CustomLineChart.tsx
+++ linechart/src/CustomLineChart.tsx
@@ -24,23 +24,7 @@ interface CustomLineChartProps {
 
 
 
-function CustomizedAxisTick(props: any) {
-  const { x, y, stroke, payload } = props;
-  return (
-    <g transform={`translate(${x},${y})`}>
-      <text
-        x={0}
-        y={0}
-        dy={16}
-        textAnchor="end"
-        fill="#666"
-        transform="rotate(-90)"
-      >
-        {payload.value}
-      </text>
-    </g>
-  );
-}
+
 
 function CustomLineChart({ lines }: CustomLineChartProps) {
   return (
